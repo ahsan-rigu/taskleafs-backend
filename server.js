d2816a4316fbafc4b52d22b1720f29dcab0c8a89
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-09-15 14:46:19 +0530

    fix cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://taskleafs.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
